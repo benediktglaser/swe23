b0973c85692261c7505abd37e88d12cb8e93a3cc
Medin Jasharoski <csaz6504@student.uibk.ac.at>
2023-04-07 19:41:44 +0200

    handling min max input

--- src/main/java/at/qe/g1t2/ui/controllers/SensorDataTypeInfoController.java
+++ src/main/java/at/qe/g1t2/ui/controllers/SensorDataTypeInfoController.java
@@ -5,17 +5,23 @@ import at.qe.g1t2.model.SensorDataTypeInfo;
 import at.qe.g1t2.model.SensorStation;
 import at.qe.g1t2.services.SensorDataTypeInfoService;
 import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.component.UIInput;
+import jakarta.faces.context.FacesContext;
 import org.jboss.logging.annotations.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.awt.event.ActionEvent;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 @Scope("view")
+@RequestScoped
 public class SensorDataTypeInfoController {
 
 
@@ -25,10 +31,12 @@ public class SensorDataTypeInfoController {
     private SensorDataTypeInfo type;
 
     private SensorDataType sensorDataType;
+
     @Transactional
     public void resetSensorDataTypeInfoController(){
         sensorDataType = null;
     }
+
     @Transactional
     public SensorDataTypeInfo getType() {
         if(type == null){
@@ -40,16 +48,17 @@ public class SensorDataTypeInfoController {
     public List<SensorDataType> getAllTypes(){
         return Arrays.stream(SensorDataType.values()).toList();
     }
+
     public void setType(SensorDataTypeInfo type) {
         this.type = type;
     }
+
     @Transactional
     public void save(SensorStation sensorStation){
-        System.out.println(sensorStation + "here save");
-        System.out.println(sensorDataType + "here save");
         type.setType(sensorDataType );
-
         sensorDataTypeInfoService.save(sensorStation,type);
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(type.getType() + ": Min value set to " + type.getMinLimit() + " and the max value is set to " + type.getMaxLimit()));
         type = null;
     }
     @Transactional
--- src/main/java/at/qe/g1t2/ui/controllers/SensorDataTypeListController.java
+++ src/main/java/at/qe/g1t2/ui/controllers/SensorDataTypeListController.java
@@ -7,6 +7,8 @@ import at.qe.g1t2.services.CollectionToPageConverter;
 import at.qe.g1t2.services.SensorDataTypeInfoService;
 import at.qe.g1t2.services.SensorStationService;
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.persistence.criteria.Path;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -28,16 +30,17 @@ public class SensorDataTypeListController{
     SensorStationService sensorStationService;
 
     private SensorStation sensorStation;
+
     @Transactional
-
     public List<SensorDataTypeInfo> getAllSensorDataTypeBySensorStation(){
-
         return sensorDataTypeInfoService.getAllSensorDataTypeInfosBySensorStation(sensorStation);
     }
+
     @Transactional
     public SensorStation getSensorStation() {
         return sensorStationService.loadSensorStation(this.sensorStation.getId());
     }
+
     @Transactional
     public void setSensorStation(SensorStation sensorStation) {
         this.sensorStation = sensorStationService.loadSensorStation(sensorStation.getId());;
