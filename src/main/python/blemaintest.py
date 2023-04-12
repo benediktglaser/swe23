069a8f5efb6d8adfd36fe7d1814ecb9461c23a99
Andreas Kirchmair <csba2047@student.uibk.ac.at>
2023-04-12 16:26:57 +0200

    Use BleakScanner.start() with detection_callback instead of easyMethods and prepare for REST-interaction; Threading per SensorStation (for Verification-polling and Services)

--- src/main/python/blemaintest.py
+++ src/main/python/blemaintest.py
@@ -1,4 +1,5 @@
 # This file is only used for testing manually if ble works
+import asyncio
 
 import myble
 import dbconnection as db
@@ -9,13 +10,4 @@ if __name__ == '__main__':
     path = "database.db"
     db.delete_database(path)
     conn = db.create_database(path)
-    # myble.ble_thread(conn)
-    t1 = Thread(target=myble.ble_thread, args=[conn])
-    t2 = Thread(target=myble.ble_thread, args=[conn])
-    t1.start()
-    print("1started")
-    t2.start()
-    print("Both started")
-    t1.join()
-    print("1 joined")
-    t2.join()
+    myble.ble_function(conn)
--- src/main/python/myble.py
+++ src/main/python/myble.py
@@ -1,79 +1,110 @@
 import asyncio
-import sqlite3
+import time
 import threading
+from threading import Thread
 from bleak import BleakScanner, BLEDevice, BleakClient
 from bleak.backends.scanner import AdvertisementData
-from dbconnection import insert_sensor_data
 
 device_name_prefix = "SensorStation G1T2"
 # all already connected SensorStations
 DEVICES = []
+# Database connection
+CONN = None
 # mutex on DEVICES
 lock = threading.Lock()
 
 
-# TODO: don't use easy method of bleakScanner because it doesn't work with multiple devices at the same time
 # Filtering for connection
 # Only connect to devices named "SensorStation G1T2 [X][X]X" - XXX being the DIP-Id.
 # DIP-Id may not be duplicate
-def filter_func(device: BLEDevice, advertisement: AdvertisementData) -> bool:
+def is_sensor_station(advertisement: AdvertisementData) -> bool:
+    """
+
+    :param advertisement: Scanned Advertisement
+    :return: bool
+    """
     if advertisement.local_name is None or not advertisement.local_name.startswith(device_name_prefix):
         return False
 
     lock.acquire()
     if any(d.name == advertisement.local_name for d in DEVICES):
+        lock.release()
         return False
     return True
 
 
-async def establish_connection():
-    device = await BleakScanner.find_device_by_filter(filter_func)  # could also have timeout
-    if device is None:
-        print("ERROR: Could not find a device")
-        lock.release()
-        return
+async def my_callback(device: BLEDevice, advertisement: AdvertisementData):
+    if is_sensor_station(advertisement):
+        await establish_connection(device)
 
+
+async def scan_for_devices():
+    scanner = BleakScanner(detection_callback=my_callback)
+    await scanner.start()
+    timeout = time.time() + 60 * 5
+    while time.time() <= timeout:
+        # Poll Pairing-Mode from Webserver
+        await asyncio.sleep(1)
+    await scanner.stop()
+
+
+async def establish_connection(device):
+    print("Establish Connection")
     DEVICES.append(device)
     lock.release()
-
-    return device
-
-
-async def call_services(conn, client):
-    print("Inside call_services()")
-    print(DEVICES)
-    await asyncio.sleep(3)
+    thread = Thread(target=ble_thread, args=[device])
+    thread.start()
 
 
-async def ble_function(conn):
-    """Function which establishes connection and calls ble services
-
-    :param conn: Connection to the Database
-    :type conn: sqlite3.Connection
-
-    :returns: Nothing
+async def call_services(device: BLEDevice):
     """
 
-    device = await establish_connection()
-    if device is None:
-        return
+    :param device: SensorStation
+    :type device: BLEDevice
+    """
     # client == sensor_station
     async with BleakClient(device) as client:
         print("Connected to device {0}".format(device.name))
-        await call_services(conn, client)
+        for i in range(3):
+            print(f"Service {i}")
+            await asyncio.sleep(3)
         print("INFO: Disconnecting from device {0} ...".format(device.name))
     print("INFO: Disconnected from device {0}".format(device.name))
 
 
-def ble_thread(conn):
-    """Thread function for each sensor_station
+def ble_function(conn):
+    """
+    Function which set the DB-Connection and starts scanning
 
     :param conn: Connection to the Database
     :type conn: sqlite3.Connection
-
-    :returns:
-        Nothing
     """
 
-    asyncio.run(ble_function(conn))
+    global CONN
+    CONN = conn
+    asyncio.run(scan_for_devices())
+
+
+def ble_thread(device: BLEDevice):
+    """Thread function for each sensor_station which polls for Verification and calls services
+
+    :param device: SensorStation
+    :type device: BLEDevice
+    """
+
+    verified = False
+    # TODO: send device to Webserver and poll for verification
+    # rest-request with MAC and dipId
+    # check returnBody if SensorStation is available (not already advertised by other AP)
+    timeout = time.time() + 60*5
+    while (not verified) and time.time() <= timeout:
+        # poll if device is verified for this AccessPoint
+        print("Poll for Verification")
+        verified = True
+        pass
+    # timed out
+    if time.time() > timeout:
+        print("ERROR: BLE-Connection timed out")
+        return
+    asyncio.run(call_services(device))
     print("Thread finished")
