6b7e67dfc8418b2e72a382061013c2133d87a5bc
Jona Mueller <Jona.Mueller@student.uibk.ac.at>
2023-04-14 18:47:05 +0200

    small changes for graphs in frontend

--- src/main/java/at/qe/g1t2/repositories/SensorDataRepository.java
+++ src/main/java/at/qe/g1t2/repositories/SensorDataRepository.java
@@ -15,7 +15,7 @@ import java.util.List;
  * Repository for managing {@link SensorData} entities.
  */
 
-public interface SensorDataRepository extends JpaRepository<SensorData, String>,JpaSpecificationExecutor<SensorData> {
+public interface SensorDataRepository extends JpaRepository<SensorData, String>, JpaSpecificationExecutor<SensorData> {
 
     SensorData findSensorDataById(String uuid);
 
@@ -24,17 +24,23 @@ public interface SensorDataRepository extends JpaRepository<SensorData, String>,
     List<SensorData> findBySensorStationId(String id);
 
     List<SensorData> findSensorDataBySensorStation(SensorStation sensorStation);
-    List<SensorData> findSensorDataBySensorStationAndTypeOrderByCreateDate(SensorStation sensorStation,SensorDataType type);
+
+    List<SensorData> findSensorDataBySensorStationAndTypeOrderByCreateDate(SensorStation sensorStation, SensorDataType type);
+
     List<SensorData> findSensorDataBySensorStationOrderByCreateDate(SensorStation sensorStation);
 
     SensorData findFirstBySensorStationAndTypeAndCreateDate(SensorStation sensorStation, SensorDataType type, LocalDateTime createDate);
 
-    List<SensorData> findSensorDataBySensorStationAndType(SensorStation sensorStation,SensorDataType sensorDataType);
-    List<SensorData> findSensorDataBySensorStationAndCreateDateBetweenOrderByCreateDate(SensorStation sensorStation,LocalDateTime start,LocalDateTime end);
-    @Query("SELECT u.timestamp, u.measurement from SensorData u where u.sensorStation = :sensorStation order by u.createDate")
+    List<SensorData> findSensorDataBySensorStationAndType(SensorStation sensorStation, SensorDataType sensorDataType);
+
+    List<SensorData> findSensorDataBySensorStationAndCreateDateBetweenOrderByCreateDate(SensorStation sensorStation, LocalDateTime start, LocalDateTime end);
+
+    @Query("SELECT u.timestamp, u.measurement from SensorData u where u.sensorStation = :sensorStation order by u.timestamp")
     List<Object[]> getSensorDataBySensorStation(SensorStation sensorStation);
-    @Query("SELECT u.timestamp, u.measurement from SensorData u where u.sensorStation = :sensorStation and u.type = :sensorDataType order by u.createDate")
-    List<Object[]> getSensorDataBySensorStationAndType(@Param("sensorStation")SensorStation sensorStation,@Param("sensorDataType") SensorDataType sensorDataType);
-    @Query("SELECT u.timestamp, u.measurement from SensorData u where u.sensorStation = :sensorStation and u.type = :sensorDataType and u.createDate >= :lastDate order by u.createDate")
-    List<Object[]> getNewSensorDataBySensorStationAndType(@Param("sensorStation")SensorStation sensorStation,@Param("sensorDataType") SensorDataType sensorDataType, @Param("lastDate") LocalDateTime lastDate);
+
+    @Query("SELECT u.timestamp, u.measurement from SensorData u where u.sensorStation = :sensorStation and u.type = :sensorDataType order by u.timestamp")
+    List<Object[]> getSensorDataBySensorStationAndType(@Param("sensorStation") SensorStation sensorStation, @Param("sensorDataType") SensorDataType sensorDataType);
+
+    @Query("SELECT u.timestamp, u.measurement from SensorData u where u.sensorStation = :sensorStation and u.type = :sensorDataType and u.timestamp > :lastDate order by u.timestamp")
+    List<Object[]> getNewSensorDataBySensorStationAndType(@Param("sensorStation") SensorStation sensorStation, @Param("sensorDataType") SensorDataType sensorDataType, @Param("lastDate") LocalDateTime lastDate);
 }
